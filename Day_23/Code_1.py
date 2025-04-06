5ac5cf5a12d32e23a1bc76e4803706ff95f575b3
Dipanshu Maddheshiya <70335021+dm9450-cloud@users.noreply.github.com>
2025-04-06 21:51:53 +0530

    Day_23 practice question

---
+++ Day_23/Code_1.py
@@ -0,0 +1,13 @@
+# a=0
+# n=int(input("Enter the number: "))
+# for i in range(n):
+#     for j in range(n):
+#         a+=1
+# print(a)
+
+
+a=0
+n=int(input("Enter the number: "))
+for i in range(n):
+    a+=n
+print(a)
